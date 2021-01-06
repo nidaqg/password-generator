0dec9801a074cef46613b569be1dca49e3c69155
nqg <nidaghuman@gmail.com>
2021-01-06 16:53:30 -0500

    write up psuedo code for the the Javascript page

--- script.js
+++ script.js
@@ -8,7 +8,27 @@ function writePassword() {
 
   passwordText.value = password;
 
+//Add event listener to execute writePassword function when button is clicked
+
+//When writePassword executes, first execute nested function generatePassword()
+//generatePassword will start series of prompt/confirm boxes
+// first prompt: Required lenght of password (minimum 8 and max 128 characters)
+// second confirm box: Do you want to use upperCase letters? yes or no
+//third confirm box: Do you want to use lowerCase letters? yes or no
+//fourth confirm box: Do you want to use numbers? yes or no
+//fifth confirm box: Do you want to use special characters? yes or no
+
+//validate input from prompt/confirm boxes
+
+//start function that will take selected input types to generate a password
+
+//When password has been generated, display it in the textbox (id = "password")
+
+
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
