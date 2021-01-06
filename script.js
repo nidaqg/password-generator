9b8dbca56dae007ee4a2c254fc299e0dbd64a04c
nqg <nidaghuman@gmail.com>
2021-01-06 17:25:57 -0500

    added code snippets for prompt boxes + created object where each property is a function returning random lowercase, uppercase, number or special character

--- script.js
+++ script.js
@@ -8,6 +8,11 @@ function writePassword() {
 
   passwordText.value = password;
 
+
+
+
+  
+
 //Add event listener to execute writePassword function when button is clicked
 
 //When writePassword executes, first execute nested function generatePassword()
@@ -32,3 +37,49 @@ generateBtn.addEventListener("click", writePassword);
 
 
 
+//prompt boxes to get password criteria
+
+var passwordLenght = parseInt(lenghtInput);
+     var lenghtInput = prompt("How many characters long do you want the password to be? Choose between 8 and 128 charcters");
+var includeLowerCase = confirm("Include Lowercase letters? Click ok to include, click cancel to not include");
+var includeUpperCase = confirm("Include Uppercase letters? Click ok to include, click cancel to not include");
+var includeNumber = confirm("Include numbers? Click ok to include, click cancel to not include");
+var includeSpecialChar = confirm("Include Special Characters? Click ok to include, click cancel to not include");
+
+
+
+
+
+
+
+//Create an object with each property value being a function, each generating a random character 
+//from lowercase, uppercase, numbers and special characters
+var randomCharacter = {
+  lower: yesLowerCase,
+  upper: yesUpperCase,
+  num: yesNumber,
+  specialChar: yesSpecialCharacters
+};
+
+
+//this function will return a random lowercase letter using the HTML character set
+ function yesLowerCase () {
+   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+ }
+
+ //this function will return a random uppercase letter using the HTML character set
+ function yesUpperCase () {
+   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+ }
+
+
+  //this function will return a random numeric character between 0 and 9 using the HTML character set
+ function yesNumber () {
+   return String.fromCharCode(Math.floor(Math.random() * 10) +48);
+ }
+
+ //this function will loop through string of special characters to return random character
+ function yesSpecialCharacters () {
+   var specialCharacters = '!@#$%^&*(){}[]=<>/,.|~?';
+   return specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
+ }
