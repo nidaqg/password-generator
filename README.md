1b6f7bdaacd11af640b01ee802281888f30cd9a5
nqg <nidaghuman@gmail.com>
2021-01-08 21:17:04 -0500

    Fixed minor errors, fixed js file comments, added README

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # password-generator
 Random Password Generator
+
+This project called for the creation of a random password generator that could be used to generate a secure password based on criteria choosen by the user.
+
+The application has a simple, responsive interface with a single button for the user to click to generate a password. Upon clicking the button, a prompt box will ask the user to enter the desired password length (between 8 and 128 characters) followed by a series of confirm boxes to choose the character types to include (options are lowecase, uppercase, numbers and special characters). Once the user has chosen the desired criteria, the application will validate the input (make sure that a number has been entered for the length prompt and that it is between 8 and 128 characters, atleast one character types has been entered). 
+
+Once valid input has been entered, the application will generate a random password that includes all the character types chosen by the user. The generated password will be displayed in the text box on the page. 
+
+For the creation of this application, very simple html and css was used and all the actual functionality of the app was achieved through the use of Javascript. I realized early on that there were multiple ways to achieve the desired result therefore I endeavored to get the result I wanted using the simplest and cleanest code I could.
+
+The finished application has the following UI:
+
+
+
+The GitHub repository of the application can be found at the following link:
+
+https://github.com/nidaqg/password-generator 
+
+The deployed application can be found at the following link:
+
+
+
--- script.js
+++ script.js
@@ -12,21 +12,20 @@ function writePassword() {
   passwordText.value = password;
 }
 
-//function called to actually determine the password based on user chosen criteria
+//The function called to actually determine the password based on user chosen criteria
 function generatePassword() {
 
   //prompt and confirm boxes to get password criteria. Placed within the generatePassword() function to be called first
-  var lengthInput = prompt("How many characters long do you want the password to be? Choose between 8 and 128 characters");
+  var lengthInput = prompt("Please input desired password length. Choose between 8 and 128 characters");
   var passwordLength = parseInt(lengthInput); //transform password lenght input into integar value
 //if statement to check validity of password length input
-if (Number.isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128)  {
+  if (Number.isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128)  {
   alert("invalid password input or length, try again!");
 } else {
   var includeLowerCase = confirm("Include Lowercase letters? Click ok to include, click cancel to not include");
   var includeUpperCase = confirm("Include Uppercase letters? Click ok to include, click cancel to not include");
   var includeNumber = confirm("Include numbers? Click ok to include, click cancel to not include");
   var includeSpecialChar = confirm("Include Special Characters? Click ok to include, click cancel to not include");
-
 
 
   //declare variables with all possible options for lowercase letters, uppercase letters, numbers and special characters
@@ -36,13 +35,11 @@ if (Number.isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) 
   var specialChar = "!@#$%^&*(){}[]=<>/,.|~?";
 
 
-
-
   //use if statement to return alert box in case no criteria is choosen
   if (includeLowerCase === false && includeUpperCase === false && includeNumber === false && includeSpecialChar === false) {
     return alert("no criteria choosen!");
   } else {
-    //Declare variable for all possible characters to draw from for password generation
+    //Declare variable that will hold all possible characters to draw from for password generation
     //for each password criteria response, if user response = true, add to charOptions variable, if false don't include
     var charOptions = '';
     if (includeLowerCase === true) {
@@ -62,7 +59,7 @@ if (Number.isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) 
   //initialze variable to hold generated password
   var finalPassword = "";
 
-  //for loop to add random character from charOptions variable to pwd variable till choosen password lenght is reached
+  //for loop to add random characters from charOptions variable to finalPassword variable till choosen password lenght is reached
   for (var i = 0; i < passwordLength; i++) {
     finalPassword += charOptions.charAt(Math.floor(Math.random() * charOptions.length));
   }
